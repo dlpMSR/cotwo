d4c0d77e22f3b55bd0b5192c51d7e3aa04075397
Zechs Merquise <pntsec_msr@outlook.com>
2023-03-15 15:28:35 +0900

    一旦ALLOWED_HOSTSを*に設定

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '(t6a-lss1omut&l&cqhhtmu7d*@0ma0!3c#i3+@l&qsj@vmtk5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

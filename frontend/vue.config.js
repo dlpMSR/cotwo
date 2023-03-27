04bbf8e412d16531414b51ac9672302c1683f174
Zechs Merquise <pntsec_msr@outlook.com>
2023-03-27 11:08:42 +0900

    vuetifyの設定追加

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      css: {
+        modules: {
+          auto: () => true
+        }
+      }
+    }
+  },
   devServer: {
     allowedHosts: 'all',
   }

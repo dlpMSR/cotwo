406570640f7a7b06a1360b710225af46af4dddd3
Zechs Merquise <pntsec_msr@outlook.com>
2023-03-30 17:44:07 +0900

    時計の余白調整機能を追加

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,43 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div :style="{ 'margin-top': margin.clockTop + 'px' }">
+      <v-row justify="center">
+        <TimeDisplay />
+      </v-row>
+    </div>
   </main>
 </template>
+
+<script lang="ts">
+import { defineComponent  } from 'vue'
+import TimeDisplay from '@/components/TimeDisplay.vue'
+
+export default defineComponent({
+  components: {
+    TimeDisplay
+  },
+
+  data() {
+    return {
+        margin: {
+          clockTop: 0,
+      }
+    }
+  },
+
+  methods: {
+    updateMargin() {
+      const iw:number = window.innerWidth
+      this.margin.clockTop = iw * 0.1
+    }
+  },
+
+  beforeMount() {
+    this.updateMargin()
+  },
+
+  mounted() {
+    window.addEventListener('resize', this.updateMargin)
+  }
+})
+</script>

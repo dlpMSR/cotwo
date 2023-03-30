964e2522bb711bef9432491d2cfde81cc655d1fd
Zechs Merquise <pntsec_msr@outlook.com>
2023-03-30 15:44:21 +0900

    最初からあったcss廃止

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import vuetify from '@/plugins/vuetify'
 import '@mdi/font/css/materialdesignicons.css'
-
-import './assets/main.css'
+import '@/assets/sass/font.scss'
 
 createApp(App).use(router).use(vuetify).mount('#app')

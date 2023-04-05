0e013a00d90395c6f101c31336654aff6d264ea0
Zechs Merquise <pntsec_msr@outlook.com>
2023-04-05 14:11:17 +0900

    チュートリアルに従ってAsyncを使うように変更

--- backend/apiv1/consumers.py
+++ backend/apiv1/consumers.py
@@ -1,20 +1,19 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+class Consumer(AsyncJsonWebsocketConsumer):
 
-class Consumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+    async def connect(self):
+        self.room_group_name = "realtime_env_ws"
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
-        # return super().disconnect(code)()
-        pass
-
-    def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+    
+    # Receive message from room group
+    async def env_data(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"message": message}))

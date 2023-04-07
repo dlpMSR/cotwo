f7dcdecff1ac69d77205cf0c55a0212cb97873a8
Zechs Merquise <pntsec_msr@outlook.com>
2023-04-07 18:53:09 +0900

    6時間前~現在のCO2濃度リストを取得するAPIを追加

--- backend/apiv1/serializers.py
+++ backend/apiv1/serializers.py
@@ -6,3 +6,10 @@ class EnvValueSerializer(serializers.ModelSerializer):
     class Meta:
         model = EnvValue
         fields = '__all__'
+
+class Co2Serializer(serializers.ModelSerializer):
+    timestamp = serializers.DateTimeField(source='created_at')
+    value = serializers.IntegerField(source='co2')
+    class Meta:
+        model = EnvValue
+        fields = ['timestamp', 'value']
--- backend/apiv1/urls.py
+++ backend/apiv1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('env_value', views.EnvValueList.as_view())
+    path('env_value', views.EnvValueList.as_view()),
+    path('co2_trend', views.Co2TrendList.as_view())
 ]

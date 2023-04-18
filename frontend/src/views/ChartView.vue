5584d746cb4dce7a9616c086b348cf0db04a083d
Zechs Merquise <pntsec_msr@outlook.com>
2023-04-18 17:44:32 +0900

    余白調整

--- frontend/src/views/ChartView.vue
+++ frontend/src/views/ChartView.vue
@@ -6,8 +6,8 @@
       </v-row>
 
       <v-row>
-        <v-col cols="12" sm="12" md="12" lg="6">
-          <div style="max-width: 600px; margin: 0 auto;" class="pr-3">
+        <v-col cols="12" sm="12" md="6" lg="6">
+          <div style="max-width: 600px; margin: 0 auto;" class="pa-2">
             <v-row>
               <div class="v-col-7">
                 <span style="font-size: 2rem;">二酸化炭素濃度</span>
@@ -21,15 +21,15 @@
               </div>
             </v-row>
             
-            <div>
+            <div class="mt-2">
               <span style="font-size: 1.7rem;">過去12時間の推移</span>
               <Co2Chart />
             </div>
           </div>
         </v-col>
 
-        <v-col cols="12" sm="12" md="12" lg="6">
-          <div style="max-width: 600px; margin: 0 auto;" class="pl-3">
+        <v-col cols="12" sm="12" md="6" lg="6">
+          <div style="max-width: 600px; margin: 0 auto;" class="pa-2">
             <v-row>
               <div class="v-col-7">
                 <span style="font-size: 2rem;">気温と湿度</span>
@@ -49,7 +49,7 @@
               </div>
             </v-row>
             
-            <div>
+            <div class="mt-2">
               <span style="font-size: 1.7rem;">過去12時間の推移</span>
               <TempHumidityChart />
             </div>

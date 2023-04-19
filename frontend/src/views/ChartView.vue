6cab5aa6bbb6924c6e9fa2f26feb4be5b96f04b6
Zechs Merquise <pntsec_msr@outlook.com>
2023-04-19 10:34:05 +0900

    7セグフォントに変更

--- frontend/src/views/ChartView.vue
+++ frontend/src/views/ChartView.vue
@@ -35,20 +35,24 @@
         <v-col cols="12" sm="12" md="6" lg="6">
           <div style="max-width: 600px; margin: 0 auto;" class="pa-2">
             <v-row>
-              <div class="v-col-7">
+              <div class="v-col-6">
                 <span style="font-size: 2rem;">気温と湿度</span>
               </div>
-              <div class="v-col-5">
+              <div class="v-col-6">
                 <div class="d-flex justify-end">
                   <span class="me-3" style="font-size: 1.3rem;">現在</span>
                   <div class="me-2">
-                    <span style="font-size: 3rem;">{{ temperature }}</span>
+                    <span class="sev-seg" style="font-size: 3rem;">{{ temperature }}</span>
                     <span class="align-self-end" style="font-size: 1.3rem;">°C</span>
                   </div>
                   <div>
-                    <span style="font-size: 3rem;">{{ humidity }}</span>
+                    <span class="sev-seg" style="font-size: 3rem;">{{ humidity }}</span>
                     <span class="align-self-end" style="font-size: 1.3rem;">%</span>
                   </div>
+                </div>
+
+                <div class="d-flex justify-end mt-1">
+                  <span v-if="lastUpdatedAt != ''" class="annotation-text">更新: {{ lastUpdatedAt }}</span>
                 </div>
               </div>
             </v-row>

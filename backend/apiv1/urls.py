7aaa5b17835190a8b3e671e1d6ff8987a506de89
Zechs Merquise <pntsec_msr@outlook.com>
2023-04-26 15:57:56 +0900

    URLのパスを変更

--- backend/apiv1/urls.py
+++ backend/apiv1/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('env_value', views.EnvValueList.as_view()),
-    path('co2_trend', views.Co2TrendList.as_view()),
-    path('co2_ma', views.Co2MovingAverageList.as_view()),
-    path('temperature_trend', views.TempTrendList.as_view()),
-    path('humidity_trend', views.HumidityTrendList.as_view()),
+    path('measurement', views.EnvValueList.as_view()),
+
+    path('trend/temperature', views.TempTrendList.as_view()),
+    path('trend/humidity', views.HumidityTrendList.as_view()),
+    path('trend/co2', views.Co2TrendList.as_view()),
+    path('trend/co2_ma', views.Co2MovingAverageList.as_view())
 ]

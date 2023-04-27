2d357627cb01ce5660a21b7106f64e3e8d009c4a
Zechs Merquise <pntsec_msr@outlook.com>
2023-04-27 15:27:07 +0900

    daemonのデモコードを仕様変更に向けて改修

--- daemon/dummy.py
+++ daemon/dummy.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import time
+import json
 import os
 import random
+import statistics
+import datetime
 # import board
 # import adafruit_scd4x
 import MySQLdb
 from dotenv import load_dotenv
-from datetime import datetime
+from pymemcache.client import PooledClient
 from pytz import timezone
 from django.conf import settings
 from channels.layers import get_channel_layer
@@ -48,6 +51,11 @@ def _set_channel_layers():
     }
 )
 
+def _set_memcached_client():
+    MEMCACHED_HOST = os.getenv('MEMCACHED_HOST')
+
+    return PooledClient(MEMCACHED_HOST, max_pool_size=4, connect_timeout=5)
+
 
 if __name__ == '__main__':
     # i2c = board.I2C()
@@ -57,27 +65,19 @@ if __name__ == '__main__':
     _set_channel_layers()
     channel_layer = get_channel_layer()
 
+    # キャッシュサーバ
+    client = _set_memcached_client()
+
     #scd4x.start_periodic_measurement()
     #print("Waiting for first measurement....")
 
     while True:
         if True:
-            temp = "%0.1f" % random.uniform(0, 50)
-            humidity = "%0.1f" % random.uniform(0, 100)
-            co2 = "%d" % int(random.uniform(400, 1500))
-            created_at = datetime.now(timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S")
-
-            print(temp, humidity, co2, created_at)
-
-            # Websocketで環境値を配信
-            async_to_sync(channel_layer.group_send)(
-                "realtime_env_ws", {
-                    "type": "env_data", "message": {
-                        "temperature": float(temp),
-                        "humidity": float(humidity),
-                        "co2": int(co2)
-                    }
-                }
+            measurement = (
+                "%0.1f" % random.uniform(0, 50),        # temp
+                "%0.1f" % random.uniform(0, 100),       # humidity
+                "%d" % int(random.uniform(400, 1500)),  # co2
+                datetime.datetime.now(timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S")    # timestamp
             )
 
             # MySQLに環境値を記録
@@ -85,10 +85,45 @@ if __name__ == '__main__':
                 INSERT INTO `env_value` (`temperature`, `humidity`, `co2`, `created_at`)
                 VALUES (%s, %s, %s, %s)
             """
-            conn = _mysql_connection()
-            with conn:
+            with _mysql_connection() as conn:
                 with conn.cursor() as cur:
-                    cur.execute(sql, (temp, humidity, co2, created_at))
+                    cur.execute(sql, measurement)
                 conn.commit()
+
+            # 過去30分の計測状況を確認
+            now_utc = datetime.datetime.now(datetime.timezone.utc)
+            thirty_mins_ago = now_utc - datetime.timedelta(minutes=30)
+            sql = """
+                SELECT `co2` FROM `env_value` WHERE `created_at` > %s ORDER BY `created_at` DESC LIMIT 100;
+            """
+            with _mysql_connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(sql, (thirty_mins_ago.strftime("%Y-%m-%d %H:%M:%S"),))
+                    co2_thirty_mins = [item[0] for item in cur.fetchall()]
+
+
+            if len(co2_thirty_mins) > 25:
+                # 補正値をキャッシュに保存
+                correction_value = {
+                    'temperature': measurement[0],
+                    'humidity': measurement[1],
+                    'co2': round(statistics.mean(co2_thirty_mins), 1),
+                    'timestamp': measurement[3] 
+                }
+
+                client.set('measurement', json.dumps(correction_value), expire=90, noreply=True)
+
+                # Websocketで補正値を配信
+                async_to_sync(channel_layer.group_send)(
+                    "realtime_env_ws", {
+                        "type": "env_data", "message": {
+                            "temperature": correction_value['temperature'],
+                            "humidity": correction_value['humidity'],
+                            "co2": correction_value['co2']
+                        }
+                    }
+                )
+
+                # TODO: 通知
 
         time.sleep(60)

06c21e4f01a609a075edd88846d7d6edf035715e
Zechs Merquise <pntsec_msr@outlook.com>
2023-06-02 15:35:22 +0900

    reverse()を使うためルーティングに名前をつけた

--- backend/environment/urls.py
+++ backend/environment/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 
     path('trend/temperature', views.TempTrendList.as_view()),
     path('trend/humidity', views.HumidityTrendList.as_view()),
-    path('trend/co2', views.Co2TrendList.as_view()),
+    path('trend/co2', views.Co2TrendList.as_view(), name='trend.co2'),
     path('trend/co2_ma', views.Co2MovingAverageList.as_view())
 ]

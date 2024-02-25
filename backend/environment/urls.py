092eda42df42d1f1107eab73cb3f2b5b9c1d85be
Zechs Merquise <pntsec_msr@outlook.com>
2024-02-26 00:43:57 +0900

    テストのため trend/temprature ルートにnameを追加した
    らしい

--- backend/environment/urls.py
+++ backend/environment/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     path('measurement', views.EnvValueList.as_view(), name='measurement'),
 
-    path('trend/temperature', views.TempTrendList.as_view()),
+    path('trend/temperature', views.TempTrendList.as_view(), name='trend.temperature'),
     path('trend/humidity', views.HumidityTrendList.as_view()),
     path('trend/co2', views.Co2TrendList.as_view(), name='trend.co2'),
     path('trend/co2_ma', views.Co2MovingAverageList.as_view(), name='trend.co2_ma')

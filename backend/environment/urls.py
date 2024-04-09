decc9fbc175d17fa73a183ab84f51fdb093fd5eb
Zechs Merquise <pntsec_msr@outlook.com>
2024-04-09 14:13:39 +0900

    テストに使うのでtrend/humidityにnameを追記

--- backend/environment/urls.py
+++ backend/environment/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path('measurement', views.EnvValueList.as_view(), name='measurement'),
 
+    # 各種トレンドデータのエンドポイント
     path('trend/temperature', views.TempTrendList.as_view(), name='trend.temperature'),
-    path('trend/humidity', views.HumidityTrendList.as_view()),
+    path('trend/humidity', views.HumidityTrendList.as_view(), name='trend.humidity'),
     path('trend/co2', views.Co2TrendList.as_view(), name='trend.co2'),
     path('trend/co2_ma', views.Co2MovingAverageList.as_view(), name='trend.co2_ma')
 ]

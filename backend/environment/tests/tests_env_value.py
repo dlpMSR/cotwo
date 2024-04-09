52e1f0ab0c4da70fbd332e61c4cc559fe3887502
Zechs Merquise <pntsec_msr@outlook.com>
2024-04-09 14:28:47 +0900

    テストの概要を説明するコメントを追加

--- backend/environment/tests/tests_env_value.py
+++ backend/environment/tests/tests_env_value.py
@@ -5,6 +5,9 @@ from ..models import EnvValue
 
 
 class EnvValueTests(TestCase):
+    """EnvValueManager.get_last_12_hours_values()のテスト
+    """
+
     def test_get_last_12_hours_with_one_record(self):
         """計測値として2時間前のデータが一件だけ保存されている場合、返されるJSONの配列の要素数は1になる。
         """

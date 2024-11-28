1e0a2c945920c34b708af5e6e8b6a98dc20b68e8
Zechs Merquise <pntsec_msr@outlook.com>
2024-11-28 15:37:18 +0900

    App.css削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Box, Tab, Tabs } from "@mui/material";
 import { Link, Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
 import Chart from "./routes/chart";
 import Home from "./routes/home";
 

5b2b1281fd438f4b926c80b561380da4425c773a
Zechs Merquise <pntsec_msr@outlook.com>
2024-12-17 18:56:12 +0900

    headerとtabのスタイルをscssから読み込むようにした

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,37 +4,29 @@ import "./app.scss";
 import Chart from "./routes/chart";
 import Home from "./routes/home";
 
-const headerStyle = {
-  position: "fixed",
-  top: 0,
-  right: 0,
-  left: 0,
-};
-
-const tabStyle = {
-  textTransform: "none",
-};
-
 const App = () => {
   const location = useLocation();
 
   return (
     <>
-      <Box sx={headerStyle}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs value={location.pathname}>
-            <Box sx={{ flexGrow: 1 }} />
-
-            <Tab label="Home" value="/" to="/" component={Link} sx={tabStyle} />
-            <Tab
-              label="Chart"
-              value="/chart"
-              to="/chart"
-              component={Link}
-              sx={tabStyle}
-            />
-          </Tabs>
-        </Box>
+      <Box className="header">
+        <Tabs value={location.pathname}>
+          <Box sx={{ flexGrow: 1 }} />
+          <Tab
+            label="Home"
+            value="/"
+            to="/"
+            component={Link}
+            className="tab-item"
+          />
+          <Tab
+            label="Chart"
+            value="/chart"
+            to="/chart"
+            component={Link}
+            className="tab-item"
+          />
+        </Tabs>
       </Box>
 
       <Routes>

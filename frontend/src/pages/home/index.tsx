a0adc2d081000f0ee05f0480b3d0ccc497123a5e
Zechs Merquise <pntsec_msr@outlook.com>
2024-12-24 19:00:14 +0900

    グリッドで時計の表示範囲を設定するように変更

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,10 +1,21 @@
 import { DigitalClockDisplay } from "@/components/DigitalClockDisplay";
+import { Grid } from "@mui/material";
+import "./home.scss";
 
 function Home() {
   return (
-    <div>
-      <DigitalClockDisplay />
-    </div>
+    <>
+      <Grid
+        container
+        spacing={0}
+        justifyContent="center"
+        sx={{ boxSizing: "border-box" }}
+      >
+        <Grid item xs={10} sm={8} md={6}>
+          <DigitalClockDisplay />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 

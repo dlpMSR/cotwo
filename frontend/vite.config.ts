c33f8561c7582cec3f7cdebf92d3c99e2ca508ef
Zechs Merquise <pntsec_msr@outlook.com>
2024-12-24 16:09:49 +0900

    importのパスのエイリアスを設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@/', replacement: `${__dirname}/src/` } 
+    ]
+  },
   server: {
     host: true,
   },

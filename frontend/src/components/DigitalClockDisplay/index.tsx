a4cbf5c462db6876c1a7d3b364e91731d3a05564
Zechs Merquise <pntsec_msr@outlook.com>
2024-12-25 13:48:50 +0900

    不要なコメントを削除

--- frontend/src/components/DigitalClockDisplay/index.tsx
+++ frontend/src/components/DigitalClockDisplay/index.tsx
@@ -17,98 +17,75 @@ export const DigitalClockDisplay = ({ width }: DigitalClockDisplayProps) => {
   });
 
   return (
-    <>
-      <Box className="clock-display">
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            marginTop: `${width * 0.0025}rem`,
-          }}
-        >
-          <Box>
-            <Typography
-              className="clock-number"
-              sx={{ fontSize: `${width * 0.009}rem` }}
-            >
-              {timeNow.year()}/{timeNow.month() + 1}/{timeNow.date()}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              className="clock-number"
-              sx={{
-                fontSize: `${width * 0.0083}rem`,
-                marginLeft: `${width * 0.001}rem`,
-              }}
-            >
-              ({timeNow.format("ddd")})
-            </Typography>
-          </Box>
+    <Box className="clock-display">
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: `${width * 0.0025}rem`,
+        }}
+      >
+        <Box>
+          <Typography
+            className="clock-number"
+            sx={{ fontSize: `${width * 0.009}rem` }}
+          >
+            {timeNow.year()}/{timeNow.month() + 1}/{timeNow.date()}
+          </Typography>
         </Box>
-
-        <Box
-          sx={{ display: "flex", flexDirection: "row", alignItems: "flex-end" }}
-        >
-          <Box>
-            <Typography
-              className="clock-number"
-              sx={{ fontSize: `${width * 0.03}rem` }}
-            >
-              {timeNow.hour()}
-            </Typography>
-          </Box>
-
-          <Box>
-            <Typography
-              className="colon"
-              sx={{ fontSize: `${width * 0.03}rem` }}
-            >
-              :
-            </Typography>
-          </Box>
-
-          <Box>
-            <Typography
-              className="clock-number"
-              sx={{ fontSize: `${width * 0.03}rem` }}
-            >
-              {timeNow.minute()}
-            </Typography>
-          </Box>
-
-          <Box>
-            <Typography
-              className="clock-number"
-              sx={{
-                fontSize: `${width * 0.018}rem`,
-                marginLeft: `${width * 0.002}rem`,
-                marginBottom: `${width * 0.001}rem`,
-              }}
-            >
-              {timeNow.second()}
-            </Typography>
-          </Box>
+        <Box>
+          <Typography
+            className="clock-number"
+            sx={{
+              fontSize: `${width * 0.0083}rem`,
+              marginLeft: `${width * 0.001}rem`,
+            }}
+          >
+            ({timeNow.format("ddd")})
+          </Typography>
         </Box>
-
-        {/* <div class="d-flex flex-row align-end">
-        <div>
-          <span class="time number">{{ hour }}</span>
-        </div>
-
-        <div>
-          <span class="time colon">:</span>
-        </div>
-
-        <div>
-          <span class="time number">{{ minute }}</span>
-        </div>
-
-        <div>
-          <span class="number second">{{ second }}</span>
-        </div>
-      </div> */}
       </Box>
-    </>
+
+      <Box
+        sx={{ display: "flex", flexDirection: "row", alignItems: "flex-end" }}
+      >
+        <Box>
+          <Typography
+            className="clock-number"
+            sx={{ fontSize: `${width * 0.03}rem` }}
+          >
+            {timeNow.hour()}
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography className="colon" sx={{ fontSize: `${width * 0.03}rem` }}>
+            :
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography
+            className="clock-number"
+            sx={{ fontSize: `${width * 0.03}rem` }}
+          >
+            {timeNow.minute()}
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography
+            className="clock-number"
+            sx={{
+              fontSize: `${width * 0.018}rem`,
+              marginLeft: `${width * 0.002}rem`,
+              marginBottom: `${width * 0.001}rem`,
+            }}
+          >
+            {timeNow.second()}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };

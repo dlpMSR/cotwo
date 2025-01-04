c375315f40eee8fc6f5020d483a2c65aeae83899
Zechs Merquise <pntsec_msr@outlook.com>
2025-01-04 12:54:02 +0900

    グラフと環境値表示が出る場所を確保

--- frontend/src/pages/chart/index.tsx
+++ frontend/src/pages/chart/index.tsx
@@ -1,3 +1,30 @@
+import { Box, Grid, Typography } from "@mui/material";
+
 export function Chart() {
-  return <></>;
+  const locationName = import.meta.env.VITE_LOCATION;
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box
+        sx={{
+          maxWidth: "1200px",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <Grid container justifyContent="center">
+          <Grid item xs={10} sm={10} md={12}>
+            <Typography variant="h4">{locationName}</Typography>
+          </Grid>
+
+          <Grid item xs={10} sm={10} md={6} sx={{ backgroundColor: "red" }}>
+            hoge
+          </Grid>
+          <Grid item xs={10} sm={10} md={6} sx={{ backgroundColor: "yellow" }}>
+            hoge
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
 }

84e46036806b4cb28dad7bd27a73eb84904f706b
Zechs Merquise <pntsec_msr@outlook.com>
2025-01-08 00:44:36 +0900

    時間の感覚がグラフに反映されるように調整

--- frontend/src/components/Co2Chart/index.tsx
+++ frontend/src/components/Co2Chart/index.tsx
@@ -1,9 +1,11 @@
 import { useApiClient } from "@/utils/useApiClient";
+import { Box } from "@mui/material";
+import dayjs from "dayjs";
 import { useEffect, useState } from "react";
-import { Line, LineChart } from "recharts";
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 type Co2Trend = {
-  timestamp: string;
+  timestamp: number | string;
   value: number;
 };
 
@@ -13,7 +15,10 @@ export function Co2Chart() {
 
   useEffect(() => {
     const fetchCo2Trend = async () => {
-      const data = await get<Co2Trend[]>("/environment/trend/co2");
+      let data = await get<Co2Trend[]>("/environment/trend/co2");
+      data = data.map((item) => {
+        return { timestamp: dayjs(item.timestamp).unix(), value: item.value };
+      });
       setCo2Trend(data);
     };
 
@@ -21,15 +26,25 @@ export function Co2Chart() {
   }, []);
 
   return (
-    <>
-      <LineChart width={400} height={400} data={co2Trend}>
-        <Line
-          type="monotone"
-          dataKey="value"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    </>
+    <Box sx={{ width: "100%" }}>
+      <ResponsiveContainer width={"100%"} height={400}>
+        <LineChart
+          data={co2Trend}
+          margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
+        >
+          <XAxis
+            dataKey="timestamp"
+            domain={["dataMin", "dataMax"]}
+            tickFormatter={(unixTime) =>
+              dayjs.unix(unixTime).format("MM-DD hh")
+            }
+            interval={1}
+            type="number"
+          />
+          <YAxis />
+          <Line type="monotone" dataKey="value" stroke="#8884d8" dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
+    </Box>
   );
 }

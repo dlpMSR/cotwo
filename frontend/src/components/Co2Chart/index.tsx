b1cc08ac0154eddc1b99a60733b1f1be2346c232
Zechs Merquise <pntsec_msr@outlook.com>
2025-01-09 01:08:36 +0900

    グラフ整形 30分間移動平均の線が表示されるようにした

--- frontend/src/components/Co2Chart/index.tsx
+++ frontend/src/components/Co2Chart/index.tsx
@@ -2,7 +2,15 @@ import { useApiClient } from "@/utils/useApiClient";
 import { Box } from "@mui/material";
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 type Co2Trend = {
   timestamp: number | string;
@@ -12,37 +20,73 @@ type Co2Trend = {
 export function Co2Chart() {
   const { get } = useApiClient();
   const [co2Trend, setCo2Trend] = useState<Co2Trend[]>([]);
+  const [co2MaTrend, setCo2MaTrend] = useState<Co2Trend[]>([]);
 
   useEffect(() => {
     const fetchCo2Trend = async () => {
-      let data = await get<Co2Trend[]>("/environment/trend/co2");
-      data = data.map((item) => {
+      const [co2Response, co2MaResponse] = await Promise.all([
+        get<Co2Trend[]>("/environment/trend/co2"),
+        get<Co2Trend[]>("/environment/trend/co2_ma"),
+      ]);
+      const co2Data = co2Response.map((item) => {
         return { timestamp: dayjs(item.timestamp).unix(), value: item.value };
       });
-      setCo2Trend(data);
+      const co2MaData = co2MaResponse.map((item) => {
+        return { timestamp: dayjs(item.timestamp).unix(), value: item.value };
+      });
+
+      setCo2Trend(co2Data);
+      setCo2MaTrend(co2MaData);
     };
 
     fetchCo2Trend();
   }, []);
 
+  const color = {
+    tick: "#666666",
+    legend: "#666666",
+  };
+  const renderCustomLegendText = (value: string) => {
+    return <span style={{ fontSize: 12, color: color.legend }}>{value}</span>;
+  };
+
   return (
     <Box sx={{ width: "100%" }}>
-      <ResponsiveContainer width={"100%"} height={400}>
-        <LineChart
-          data={co2Trend}
-          margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
-        >
+      <ResponsiveContainer width={"100%"} height={255}>
+        <LineChart margin={{ top: 0, right: 20, left: 0, bottom: 0 }}>
           <XAxis
             dataKey="timestamp"
-            domain={["dataMin", "dataMax"]}
-            tickFormatter={(unixTime) =>
-              dayjs.unix(unixTime).format("MM-DD hh")
-            }
-            interval={1}
             type="number"
+            domain={["dataMin", "dataMax"]}
+            tickCount={12}
+            tick={{ stroke: color.tick }}
+            tickFormatter={(unixTime) => dayjs.unix(unixTime).format("h A")}
           />
-          <YAxis />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" dot={false} />
+          <YAxis tickCount={10} />
+          <CartesianGrid strokeDasharray="" vertical={false} />
+          <Legend
+            verticalAlign="top"
+            height={24}
+            formatter={renderCustomLegendText}
+          />
+          <Line
+            data={co2Trend}
+            name="二酸化炭素濃度 [ppm]"
+            type="monotone"
+            dataKey="value"
+            stroke="#afeeee"
+            strokeWidth={3}
+            dot={false}
+          />
+          <Line
+            data={co2MaTrend}
+            name="30分間移動平均 [ppm]"
+            type="monotone"
+            dataKey="value"
+            stroke="#20b2aa"
+            strokeWidth={3}
+            dot={false}
+          />
         </LineChart>
       </ResponsiveContainer>
     </Box>

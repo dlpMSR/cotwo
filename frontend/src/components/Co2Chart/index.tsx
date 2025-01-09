27d165d53298b16a1ed57a2c630822e0097969f7
Zechs Merquise <pntsec_msr@outlook.com>
2025-01-09 10:31:22 +0900

    fontSizeの指定を削除

--- frontend/src/components/Co2Chart/index.tsx
+++ frontend/src/components/Co2Chart/index.tsx
@@ -47,22 +47,21 @@ export function Co2Chart() {
     legend: "#666666",
   };
   const renderCustomLegendText = (value: string) => {
-    return <span style={{ fontSize: 12, color: color.legend }}>{value}</span>;
+    return <span style={{ color: color.legend }}>{value}</span>;
   };
 
   return (
     <Box sx={{ width: "100%" }}>
       <ResponsiveContainer width={"100%"} height={255}>
-        <LineChart margin={{ top: 0, right: 20, left: 0, bottom: 0 }}>
+        <LineChart margin={{ top: 0, right: 30, left: 0, bottom: 0 }}>
           <XAxis
             dataKey="timestamp"
             type="number"
             domain={["dataMin", "dataMax"]}
             tickCount={12}
-            tick={{ stroke: color.tick }}
-            tickFormatter={(unixTime) => dayjs.unix(unixTime).format("h A")}
+            tickFormatter={(unixTime) => dayjs.unix(unixTime).format("hA")}
           />
-          <YAxis tickCount={10} />
+          <YAxis tickCount={10} width={40} />
           <CartesianGrid strokeDasharray="" vertical={false} />
           <Legend
             verticalAlign="top"
@@ -71,7 +70,7 @@ export function Co2Chart() {
           />
           <Line
             data={co2Trend}
-            name="二酸化炭素濃度 [ppm]"
+            name="二酸化炭素濃度[ppm]"
             type="monotone"
             dataKey="value"
             stroke="#afeeee"
@@ -80,7 +79,7 @@ export function Co2Chart() {
           />
           <Line
             data={co2MaTrend}
-            name="30分間移動平均 [ppm]"
+            name="30分間移動平均[ppm]"
             type="monotone"
             dataKey="value"
             stroke="#20b2aa"

b05b0ba17a27c0df42c4d9b79b81a5f15ae6995f
Zechs Merquise <pntsec_msr@outlook.com>
2025-01-12 23:56:18 +0900

    vite-plugin-webfont-dlでwebフォントを読み込む設定を追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +12,10 @@ export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap'
+    ])
+  ]
 })

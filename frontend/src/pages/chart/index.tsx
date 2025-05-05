621eff769f1e698397d94676d0c991dd825ce7d0
Zechs Merquise <pntsec_msr@outlook.com>
2025-05-06 01:20:21 +0900

    socket -> socketRefを使うようにする対応

--- frontend/src/pages/chart/index.tsx
+++ frontend/src/pages/chart/index.tsx
@@ -2,7 +2,10 @@ import { Co2Chart } from "@/components/charts/Co2Chart";
 import { TempHumidChart } from "@/components/charts/TempHumidChart";
 import { CurrentCo2Display } from "@/components/currentDisplay/CurrentCo2Display";
 import { CurrentTempHumidDisplay } from "@/components/currentDisplay/CurrentTempHumidDisplay";
-import { EnvValueStreamContext } from "@/contexts/EnvValueStreamContext";
+import {
+  EnvValueStreamContext,
+  SocketStateContext,
+} from "@/contexts/EnvValueStreamContext";
 import { RefreshContext } from "@/contexts/RefreshContext";
 import {
   CurrentEnvValue,
@@ -23,7 +26,8 @@ export function Chart() {
   const locationName = import.meta.env.VITE_LOCATION;
   const { get } = useApiClient();
   const initDate = useContext(RefreshContext);
-  const socket = useContext(EnvValueStreamContext);
+  const socketRef = useContext(EnvValueStreamContext);
+  const readyState = useContext(SocketStateContext);
   const [envValue, setEnvValue] = useState<CurrentEnvValue>({
     co2: 0,
     temperature: 0,
@@ -94,16 +98,14 @@ export function Chart() {
 
     fetchEnvValue();
     fetchTrendData();
-    if (socket !== undefined) {
-      socket.addEventListener("message", updateLiveData);
+    if (socketRef?.current) {
+      socketRef.current.addEventListener("message", updateLiveData);
     }
 
     return () => {
-      if (socket !== undefined) {
-        socket.removeEventListener("message", updateLiveData);
-      }
+      socketRef?.current?.removeEventListener("message", updateLiveData);
     };
-  }, [initDate]);
+  }, [readyState, initDate]);
 
   return (
     <Box className="chart-page" sx={{ width: "100%" }}>

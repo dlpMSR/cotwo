65b2be1e024eb7c4ab4ab11d0e3125ed44155a02
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-10 18:50:08 +0900

    過去12時間以内のデータを返す処理を追加

--- backend/src/routers/environment/trend.py
+++ backend/src/routers/environment/trend.py
@@ -1,21 +1,56 @@
+from datetime import datetime, timedelta
+
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from src.database import Session
+from src.models.env_value import EnvValue
 
 router = APIRouter(prefix="/environment/trend", tags=["trend"])
 
 
-@router.get("/co2")
+class TrendDatum(BaseModel):
+    timestamp: datetime
+    value: int | float
+
+
+@router.get("/co2", response_model=list[TrendDatum])
 async def trend_co2():
-    pass
+    with Session() as session:
+        twelve_hours_ago = datetime.now() - timedelta(hours=12)
+        query = session.query(EnvValue.created_at, EnvValue.co2).filter(
+            EnvValue.created_at > twelve_hours_ago
+        )
+
+        result = query.all()
+
+    return [TrendDatum(timestamp=timestamp, value=value) for timestamp, value in result]
 
 
-@router.get("/temperature")
+@router.get("/temperature", response_model=list[TrendDatum])
 async def trend_temperature():
-    pass
+    with Session() as session:
+        twelve_hours_ago = datetime.now() - timedelta(hours=12)
+        query = session.query(EnvValue.created_at, EnvValue.temperature).filter(
+            EnvValue.created_at > twelve_hours_ago
+        )
+
+        result = query.all()
+
+    return [TrendDatum(timestamp=timestamp, value=value) for timestamp, value in result]
 
 
-@router.get("/humidity")
+@router.get("/humidity", response_model=list[TrendDatum])
 async def trend_humidity():
-    pass
+    with Session() as session:
+        twelve_hours_ago = datetime.now() - timedelta(hours=12)
+        query = session.query(EnvValue.created_at, EnvValue.humidity).filter(
+            EnvValue.created_at > twelve_hours_ago
+        )
+
+        result = query.all()
+
+    return [TrendDatum(timestamp=timestamp, value=value) for timestamp, value in result]
 
 
 def register_router(api_router: APIRouter):

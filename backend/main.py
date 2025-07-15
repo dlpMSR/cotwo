e7f0d1609d53a498ae3b74ac92a41bbc7fc7100c
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-15 17:57:15 +0900

    redis_listenerを追加

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
+import asyncio
+
 import typer
 from fastapi import FastAPI
 from src.commands import init_cli
+from src.redis import redis_listener
 from src.routers import init_api_router
 from src.routers.websocket import setup_websocket_routes, ws_router
 from src.websocket import ConnectionManager
@@ -12,6 +15,9 @@ manager = ConnectionManager()
 init_api_router(app)
 setup_websocket_routes(manager)
 app.include_router(ws_router)
+
+loop = asyncio.get_event_loop()
+loop.create_task(redis_listener(manager))
 
 
 @app.get("/")
--- backend/src/redis.py
+++ backend/src/redis.py
@@ -1,8 +1,11 @@
+import json
 import os
 from pathlib import Path
 
 import redis.asyncio as redis
 from dotenv import load_dotenv
+
+from src.websocket import ConnectionManager
 
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
@@ -15,3 +18,14 @@ redis_client = redis.Redis(
     port=REDIS_PORT,
     decode_responses=True,
 )
+
+
+async def redis_listener(manager: ConnectionManager):
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe("cotwo:env_value_broadcast")
+
+    async for message in pubsub.listen():
+        print(message)
+        if message["type"] == "message":
+            data: str = message["data"]
+            await manager.broadcast(data)

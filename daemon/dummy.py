f6ee41c567962e79b02b372e1bdbc73ecd54e76f
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-15 18:40:07 +0900

    djangoの機能を使わずに環境値をPublishするように修正

--- daemon/dummy.py
+++ daemon/dummy.py
@@ -1,88 +1,68 @@
 #!/usr/bin/env python3
 
-import time
+import datetime
 import json
 import os
 import random
-import redis
 import statistics
-import datetime
+import time
+
 # import board
 # import adafruit_scd4x
 import MySQLdb
+import redis
 from dotenv import load_dotenv
 from pytz import timezone
-from django.conf import settings
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
 
 
 def _mysql_connection():
     load_dotenv()
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = int(os.getenv('DB_PORT'))
-    DB_NAME = os.getenv('DB_NAME')
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT"))
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
 
     connection = MySQLdb.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        user=DB_USER,
-        passwd=DB_PASSWORD,
-        db=DB_NAME
+        host=DB_HOST, port=DB_PORT, user=DB_USER, passwd=DB_PASSWORD, db=DB_NAME
     )
 
     return connection
 
+
 def _set_redis_client():
     load_dotenv()
-    REDIS_HOST = os.getenv('REDIS_HOST')
-    REDIS_PORT = int(os.getenv('REDIS_PORT'))
+    REDIS_HOST = os.getenv("REDIS_HOST")
+    REDIS_PORT = int(os.getenv("REDIS_PORT"))
 
-    redis_pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0, max_connections=4)
+    redis_pool = redis.ConnectionPool(
+        host=REDIS_HOST, port=REDIS_PORT, db=0, max_connections=4
+    )
     conn = redis.StrictRedis(connection_pool=redis_pool)
 
     return conn
 
-def _set_channel_layers():
-    load_dotenv()
-    REDIS_HOST = os.getenv('REDIS_HOST')
-    REDIS_PORT = int(os.getenv('REDIS_PORT'))
 
-    settings.configure(
-    CHANNEL_LAYERS={
-        "default": {
-            "BACKEND": "channels_redis.core.RedisChannelLayer",
-            "CONFIG": {
-                "hosts": [(REDIS_HOST, REDIS_PORT)],
-            },
-        }
-    }
-)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # i2c = board.I2C()
     # scd4x = adafruit_scd4x.SCD4X(i2c)
     # print("Serial number:", [hex(i) for i in scd4x.serial_number])
 
-    _set_channel_layers()
-    channel_layer = get_channel_layer()
-
-    #scd4x.start_periodic_measurement()
-    #print("Waiting for first measurement....")
+    # scd4x.start_periodic_measurement()
+    # print("Waiting for first measurement....")
 
     while True:
         if True:
             measurement = (
-                round(random.uniform(0, 50), 1),        # temp
-                round(random.uniform(0, 100), 1),       # humidity
+                round(random.uniform(0, 50), 1),  # temp
+                round(random.uniform(0, 100), 1),  # humidity
                 "%d" % int(random.uniform(400, 1500)),  # co2
-                datetime.datetime.now(timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S")    # timestamp
+                datetime.datetime.now(timezone("UTC")).strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                ),  # timestamp
             )
             print(measurement)
-            
+
             # MySQLに環境値を記録
             sql = """
                 INSERT INTO `env_value` (`temperature`, `humidity`, `co2`, `created_at`)
@@ -108,26 +88,38 @@ if __name__ == '__main__':
             if len(co2_thirty_mins) > 25:
                 # 補正値をキャッシュに保存
                 api_value = {
-                    'temperature': measurement[0],
-                    'humidity': measurement[1],
-                    'co2': round(statistics.mean(co2_thirty_mins), 1),
-                    'timestamp': measurement[3] 
+                    "temperature": measurement[0],
+                    "humidity": measurement[1],
+                    "co2": round(statistics.mean(co2_thirty_mins), 1),
+                    "timestamp": measurement[3],
+                }
+
+                ws_value = {
+                    "type": "env_data",
+                    "message": {
+                        "temperature": measurement[0],
+                        "humidity": measurement[1],
+                        "co2": int(measurement[2]),
+                        "co2_corrected": round(statistics.mean(co2_thirty_mins), 1),
+                        "timestamp": measurement[3],
+                    },
                 }
 
                 conn = _set_redis_client()
-                conn.set('scd41:measurement', json.dumps(api_value), ex=90)
-                
+                conn.set("scd41:measurement", json.dumps(api_value), ex=90)
+                conn.publish("cotwo:env_value_broadcast", json.dumps(ws_value))
+
                 # Websocketで補正値を配信
-                async_to_sync(channel_layer.group_send)(
-                    "realtime_env_ws", {
-                        "type": "env_data", "message": {
-                            "temperature": measurement[0],
-                            "humidity": measurement[1],
-                            "co2": int(measurement[2]),
-                            "co2_corrected": round(statistics.mean(co2_thirty_mins), 1),
-                            "timestamp": measurement[3]
-                        }
-                    }
-                )
+                # async_to_sync(channel_layer.group_send)(
+                #     "realtime_env_ws", {
+                #         "type": "env_data", "message": {
+                #             "temperature": measurement[0],
+                #             "humidity": measurement[1],
+                #             "co2": int(measurement[2]),
+                #             "co2_corrected": round(statistics.mean(co2_thirty_mins), 1),
+                #             "timestamp": measurement[3]
+                #         }
+                #     }
+                # )
 
         time.sleep(60)

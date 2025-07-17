6ad571849671ff1dcea43c024066418b4781e24f
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-17 13:03:54 +0900

    最新の計測値を取得するusecaseを追加

--- backend/src/usecases/env_value_usecase.py
+++ backend/src/usecases/env_value_usecase.py
@@ -6,6 +6,10 @@ class EnvValueUsecase:
     def __init__(self, env_value_repository: EnvValueRepository):
         self.env_value_repository = env_value_repository
 
+    def get_latest_record(self) -> EnvValue:
+        result = self.env_value_repository.get_latest()
+        return result
+
     def get_last_12_hours_records(self) -> list[EnvValue]:
         return self.env_value_repository.get_last_n_hours_data(n=12)
 

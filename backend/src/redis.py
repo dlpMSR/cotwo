88418160c4c746eb8576b23640838c673593c66f
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-17 12:50:11 +0900

    asyncioでない方のredisを使うように変更

--- backend/src/redis.py
+++ backend/src/redis.py
@@ -1,8 +1,7 @@
-import json
 import os
 from pathlib import Path
 
-import redis.asyncio as redis
+import redis
 from dotenv import load_dotenv
 
 from src.websocket import ConnectionManager
@@ -13,19 +12,13 @@ load_dotenv(dotenv_path=env_path)
 REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
 
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    decode_responses=True,
-)
+pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT)
+redis_client = redis.Redis(connection_pool=pool)
 
 
 async def redis_listener(manager: ConnectionManager):
     pubsub = redis_client.pubsub()
     await pubsub.subscribe("cotwo:env_value_broadcast")
-
     async for message in pubsub.listen():
-        print(message)
         if message["type"] == "message":
-            data: str = message["data"]
-            await manager.broadcast(data)
+            await manager.broadcast(message["data"])

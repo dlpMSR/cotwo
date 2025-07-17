20c1a0d7a117efc0c061a344258d40a60cd7df0a
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-17 16:27:40 +0900

    asyncioのredis_clientを分けた

--- backend/src/redis.py
+++ backend/src/redis.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 import redis
+import redis.asyncio as aioredis
 from dotenv import load_dotenv
 
 from src.websocket import ConnectionManager
@@ -13,11 +14,15 @@ REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
 
 pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT)
-redis_client = redis.Redis(connection_pool=pool)
+redis_client = redis.Redis(connection_pool=pool, decode_responses=True)
+
+aioredis_client = aioredis.Redis(
+    host=REDIS_HOST, port=REDIS_PORT, decode_responses=True
+)
 
 
 async def redis_listener(manager: ConnectionManager):
-    pubsub = redis_client.pubsub()
+    pubsub = aioredis_client.pubsub()
     await pubsub.subscribe("cotwo:env_value_broadcast")
     async for message in pubsub.listen():
         if message["type"] == "message":

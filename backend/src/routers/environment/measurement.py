6818b99398a3c9f12f98952a6a729afd07792be8
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-17 13:02:55 +0900

    redisからのデータ取得などをEnvValueRepositoryの実装の方に移動したので削除

--- backend/src/routers/environment/measurement.py
+++ backend/src/routers/environment/measurement.py
@@ -1,28 +1,33 @@
-import json
+from fastapi import APIRouter, Depends
 
-from fastapi import APIRouter
-
-from src.redis import redis_client
+from src.repositories.env_value_repository import EnvValueRepository
+from src.repositories.redis.env_value_repository_impl import EnvValueRepositoryImpl
 from src.schemas.responses.env_value_response_shema import LatestEnvValue
+from src.usecases.env_value_usecase import EnvValueUsecase
 
 router = APIRouter(prefix="/environment", tags=["measurement"])
 
 
-@router.get("/measurement")
-async def measurement():
-    try:
-        measurement = await redis_client.get("scd41:measurement")
-    except Exception as e:
-        print(e)
-        raise
+# DI関数
+def get_env_value_repository():
+    return EnvValueRepositoryImpl()
 
-    if measurement:
-        values = json.loads(measurement)
-        return LatestEnvValue(
-            temperature=values["temperature"],
-            humidity=values["humidity"],
-            co2=values["humidity"],
-        )
+
+def get_env_value_usecase(
+    env_value_repository: EnvValueRepository = Depends(get_env_value_repository),
+):
+    return EnvValueUsecase(env_value_repository=env_value_repository)
+
+
+@router.get("/measurement")
+async def measurement(usecase: EnvValueUsecase = Depends(get_env_value_usecase)):
+    record = usecase.get_latest_record()
+
+    return LatestEnvValue(
+        temperature=record.temperature,
+        humidity=record.humidity,
+        co2=record.co2,
+    )
 
 
 def register_router(api_router: APIRouter):

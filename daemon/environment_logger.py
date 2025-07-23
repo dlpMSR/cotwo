100bcbf1991c02bcb1aa093d8a37b0a3bb405b95
Zechs Merquise <pntsec_msr@outlook.com>
2025-07-23 19:10:22 +0900

    mysqlとredisのclientを用意する関数名を修正

--- daemon/environment_logger.py
+++ daemon/environment_logger.py
@@ -70,7 +70,7 @@ class MockSensorRepository(SensorRepository):
         )
 
 
-def _mysql_connection():
+def get_mysql_connection():
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = int(os.getenv("DB_PORT"))
     DB_NAME = os.getenv("DB_NAME")
@@ -82,7 +82,7 @@ def _mysql_connection():
     )
 
 
-def _set_redis_client():
+def get_redis_client():
     REDIS_HOST = os.getenv("REDIS_HOST")
     REDIS_PORT = int(os.getenv("REDIS_PORT"))
     return redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
@@ -106,7 +106,7 @@ if __name__ == "__main__":
                 INSERT INTO `env_value` (`temperature`, `humidity`, `co2`, `created_at`)
                 VALUES (%s, %s, %s, %s)
             """
-            with _mysql_connection() as conn:
+            with get_mysql_connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(
                         sql,
@@ -124,7 +124,7 @@ if __name__ == "__main__":
             sql = """
                 SELECT `co2` FROM `env_value` WHERE `created_at` > %s ORDER BY `created_at` DESC LIMIT 100;
             """
-            with _mysql_connection() as conn:
+            with get_mysql_connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(sql, (thirty_mins_ago.strftime("%Y-%m-%dT%H:%M:%SZ"),))
                     co2_thirty_mins = [item[0] for item in cur.fetchall()]
@@ -139,7 +139,7 @@ if __name__ == "__main__":
                     "co2_corrected": round(statistics.mean(co2_thirty_mins), 1),
                     "timestamp": measurement.timestamp.strftime("%Y-%m-%dT%H:%M:%SZ"),
                 }
-                conn = _set_redis_client()
+                conn = get_redis_client()
                 conn.set("cotwo:env_value_measurement", json.dumps(output), ex=90)
                 conn.publish(
                     "cotwo:env_value_broadcast", json.dumps({"message": output})
